becfa9d713b755de2a11e8e80d69f601b6b941f9
mpi31415 <maximilian.ponomarev@gmail.com>
2022-07-06 03:20:00 +0200

    db update

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config.from_mapping(
 )
 
 db = SQLAlchemy(app)
-migrate.init_app(app, db)
+
 
 
 class Users(db.Model):
@@ -122,6 +122,7 @@ def index():
 
 
 if __name__ == '__main__':
-   # db.init_app(app)
-    db.create_all()
+    db.init_app(app)
+    #db.create_all()
+    migrate.init_app(app, db)
     app.run(threaded=True, port=5000)
