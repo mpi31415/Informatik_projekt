2c95643f52b979def52a338c7fb74d820b52dad6
mpi31415 <maximilian.ponomarev@gmail.com>
2022-07-06 15:06:19 +0200

    quest  update

--- quests.py
+++ quests.py
@@ -1,37 +1,12 @@
 import random
 
 
-def ask_for_int(message: str) -> int:
-    number = None
-    while type(number) != int:
-        try:
-            number = int(input(message))
-        except ValueError:
-            print("Das ist keine Ganzzahl!")
-    return number
-
-
 def generate_math():
-    punkte = 0
-    combo_true = 1
-    combo_false = 1
     for i in range(1, 11):
-        print(f"Runde {i}:")
         a = random.randint(1, 10)
         b = random.randint(1, 10)
         operator = random.choice(["+", "-", "*", "%", "//", "**"])
 
-        user_input = ask_for_int(f"Wie viel ist {a} {operator} {b} ? ")
+        user_input = f"Wie viel ist {a} {operator} {b} ? "
 
-        if user_input == eval(f"{a}{operator}{b}"):
-            punkte += i * combo_true ** 2
-            combo_true += 100
-            combo_false = 1
-            print("Korrekt")
-        else:
-            punkte -= i * combo_false ** 2
-            combo_false += 100
-            combo_true = 1
-            print("Falsch")
-
-        print(f"Punkte: {punkte}\n")
+        return {user_input: eval(f"{a}{operator}{b}")}
