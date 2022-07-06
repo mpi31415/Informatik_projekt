e7d400a8f274da4344f1bd7dd1c970ba57427f4b
mpi31415 <maximilian.ponomarev@gmail.com>
2022-07-06 03:01:33 +0200

    db update

--- app.py
+++ app.py
@@ -12,10 +12,14 @@ import json
 
 app = Flask(__name__)
 
+
 app.config.from_mapping(
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev_key',
-    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
-                            'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite').replace("postgres://", "postgresql://", 1),
+    SQLALCHEMY_DATABASE_URI=((os.environ.get('DATABASE_URL') or
+                              'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite'))).replace(
+        "postgres://",
+        "postgresql://",
+        1),
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
 
